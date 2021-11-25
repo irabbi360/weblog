f0d3dfb68f3b778c81813c0068d5f4ed1e0a06ef
irabbi360 <fazle08rabbi@gmail.com>
2021-11-25 22:04:34 +0600

    version update 6 to 8 & other issues fixed

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //insert a user
+        DB::table('users')->insert([
+            'name' => 'Mr Admin',
+            'email' => 'admin@demo.com',
+            'password' => Hash::make(12345678),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        // $this->call(UsersTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
+        $this->call(PostSeeder::class);
+    }
+}

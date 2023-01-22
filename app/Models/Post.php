a7025b6e7f218cd53f8496b7f5b5ba34a81d3f8f
irabbi360 <fazle08rabbi@gmail.com>
2023-01-22 23:58:57 +0600

    post category model relation updated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $guarded = [];
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+        'category_id',
+        'is_published',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($post) {
+            if (is_null($post->user_id)) {
+                $post->user_id = auth()->user()->id;
+            }
+        });
+
+        static::deleting(function ($post) {
+            $post->comments()->delete();
+            $post->tags()->detach();
+        });
+    }
 
     public function category()
     {
@@ -16,5 +38,35 @@ class Post extends Model
 
     public function user(){
         return $this->belongsTo(User::class,'created_by');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where('is_published', true);
+    }
+
+    public function scopeDrafted($query)
+    {
+        return $query->where('is_published', false);
+    }
+
+    public function getPublishedAttribute()
+    {
+        return ($this->is_published) ? 'Yes' : 'No';
+    }
+
+    public function getEtagAttribute()
+    {
+        return hash('sha256', "product-{$this->id}-{$this->updated_at}");
     }
 }

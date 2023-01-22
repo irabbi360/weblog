d11f153c44bc3c8f4fc7aaad220c34f61bc33a34
irabbi360 <fazle08rabbi@gmail.com>
2023-01-22 23:58:57 +0600

    post category model relation updated

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,6 +8,14 @@ class Category extends Model
 {
     protected $guarded = [];
 
+    protected static function boot()
+    {
+        parent::boot();
+        static::deleting(function ($category) {
+            $category->posts()->delete();
+        });
+    }
+
     public function posts()
     {
         return $this->hasMany(Post::class,'category_id');

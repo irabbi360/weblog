ac2ffa4a9afa31e937e098ea4c50f138ca29113a
Fazle Rabbi <fazle08rabbi@gmail.com>
2023-01-24 17:22:08 +0600

    store request updated

--- app/Http/Requests/StorePermissionRequest.php
+++ app/Http/Requests/StorePermissionRequest.php
@@ -13,18 +13,18 @@ class StorePermissionRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array
+     * @return array<string, mixed>
      */
     public function rules()
     {
         return [
-            //
+            'name' => 'required'
         ];
     }
 }

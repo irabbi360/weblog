e96a0e1a4c32974d91b0c256a22def7b6b077122
irabbi360 <fazle08rabbi@gmail.com>
2023-01-26 23:21:37 +0600

    breadcrumbs added

--- resources/views/admin/includes/breadcrumb.blade.php
+++ resources/views/admin/includes/breadcrumb.blade.php
@@ -1,6 +1,7 @@
-<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-    <h4>Dashboard</h4>
-    <div class="btn-toolbar mb-2 mb-md-0">
+
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-1 mb-3 border-bottom">
+    {{ Breadcrumbs::render() }}
+    {{--<div class="btn-toolbar mb-2 mb-md-0">
         <div class="btn-group me-2">
             <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
             <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
@@ -9,5 +10,5 @@
             <span data-feather="calendar" class="align-text-bottom"></span>
             This week
         </button>
-    </div>
+    </div>--}}
 </div>
---
+++ routes/breadcrumbs.php
@@ -0,0 +1,91 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Diglactic\Breadcrumbs\Breadcrumbs;
+use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+use Spatie\Permission\Models\Role;
+
+Breadcrumbs::for('admin.index', function (BreadcrumbTrail $trail): void {
+    $trail->push('Dashboard', route('admin.index'));
+});
+Breadcrumbs::for('admin.users.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.index');
+    $trail->push('Users', route('admin.users.index'));
+});
+// Post
+Breadcrumbs::for('admin.posts.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.index');
+    $trail->push('Posts', route('admin.posts.index'));
+});
+Breadcrumbs::for('admin.posts.create', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.posts.index');
+
+    $trail->push('Add new post', route('admin.posts.create'));
+});
+Breadcrumbs::for('admin.posts.edit', function (BreadcrumbTrail $trail, Post $post): void {
+    $trail->parent('admin.posts.index');
+
+    $trail->push($post->title, route('admin.posts.edit', $post));
+});
+// Category
+Breadcrumbs::for('admin.categories.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.index');
+    $trail->push('Categories', route('admin.categories.index'));
+});
+Breadcrumbs::for('admin.categories.create', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.categories.index');
+
+    $trail->push('Add new category', route('admin.posts.create'));
+});
+Breadcrumbs::for('admin.categories.edit', function (BreadcrumbTrail $trail, Category $post): void {
+    $trail->parent('admin.categories.index');
+
+    $trail->push($post->title, route('admin.categories.edit', $post));
+});
+// Tag
+Breadcrumbs::for('admin.tags.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.index');
+    $trail->push('Tags', route('admin.tags.index'));
+});
+Breadcrumbs::for('admin.tags.create', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.tags.index');
+
+    $trail->push('Add new tag', route('admin.tags.create'));
+});
+Breadcrumbs::for('admin.tags.edit', function (BreadcrumbTrail $trail, Tag $post): void {
+    $trail->parent('admin.tags.index');
+
+    $trail->push($post->title, route('admin.tags.edit', $post));
+});
+// Role
+Breadcrumbs::for('admin.roles.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.index');
+    $trail->push('Roles', route('admin.roles.index'));
+});
+Breadcrumbs::for('admin.roles.create', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.roles.index');
+
+    $trail->push('Add new role', route('admin.roles.create'));
+});
+Breadcrumbs::for('admin.roles.edit', function (BreadcrumbTrail $trail, Role $post): void {
+    $trail->parent('admin.roles.index');
+
+    $trail->push($post->name, route('admin.roles.edit', $post));
+});
+// Permission
+Breadcrumbs::for('admin.permissions.index', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.index');
+    $trail->push('Permissions', route('admin.permissions.index'));
+});
+Breadcrumbs::for('admin.permissions.create', function (BreadcrumbTrail $trail): void {
+    $trail->parent('admin.permissions.index');
+
+    $trail->push('Add new permission', route('admin.permissions.create'));
+});
+Breadcrumbs::for('admin.permissions.edit', function (BreadcrumbTrail $trail, Role $post): void {
+    $trail->parent('admin.permissions.index');
+
+    $trail->push($post->name, route('admin.permissions.edit', $post));
+});

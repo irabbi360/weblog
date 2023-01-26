74d17f2080316ea60a1764c96b90fd3f1efb1133
Fazle Rabbi <fazle08rabbi@gmail.com>
2023-01-26 11:36:23 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="position-relative justify-content-center py-4">
                 <header class="text-center text-white">
                     <h1 class="fw-bold my-3">

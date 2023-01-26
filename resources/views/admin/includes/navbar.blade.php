003ec96e63a5e45695997926be3fcf6d1972db33
irabbi360 <fazle08rabbi@gmail.com>
2023-01-26 22:44:44 +0600

    Update navbar.blade.php

--- resources/views/admin/includes/navbar.blade.php
+++ resources/views/admin/includes/navbar.blade.php
@@ -17,7 +17,16 @@
             <li><a class="dropdown-item" href="#">Profile</a></li>
             <li><a class="dropdown-item" href="#">Change Password</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Logout</a></li>
+            <li>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                   onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                >
+                    {{ __('Logout') }}
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
         </ul>
     </div>
 </header>
